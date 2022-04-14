a5414c490d0a4de5f38f824503764f94d6f1002c
scrawfor51 <71202431+scrawfor51@users.noreply.github.com>
2022-04-14 17:54:30 -0400

    Add files via upload

--- OracleStrategy.py
+++ OracleStrategy.py
@@ -9,6 +9,7 @@ Created on Thu Apr  7 19:01:42 2022
 import tech_ind 
 import pandas as pd
 import numpy as np
+from matplotlib import cm
 
 class OracleStrategy:
     def __init__(self, *params, **kwparams):
@@ -28,11 +29,20 @@ class OracleStrategy:
         daily_returns.values[0, :] = np.nan
         daily_returns['Cumulative Returns'] = (daily_returns).cumsum()
         daily_returns['Max Value'] = 200000 + daily_returns['Cumulative Returns']
-                
-        print(daily_returns)
         return daily_returns
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__": 
+    data = tech_ind.get_data(start_date='2018-01-01', end_date='2019-12-31', symbols =['DIS'],  include_spy=False)
+   
+    daily_returns = (data.shift()-data) * 1000
+    daily_returns.values[0, :] = np.nan
+    daily_returns = daily_returns.cumsum()
     OS = OracleStrategy()
-    OS.test()
+    oracle = OS.test()
+    base_returns = daily_returns
+    base_returns.columns = ['Baseline']
+    base_returns['Max Value'] = oracle['Max Value'] - 200000
+    plot = base_returns.plot(title='Baseline vs. Oracle', colormap=cm.Accent)
+    plot.grid()
+    print(base_returns.std(axis=0))
