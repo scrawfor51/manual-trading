5868cca2640334922f681408f867cb20ab2e4cb2
Tugi Davaadorj <tdavaado@bowdoin.edu>
2022-04-16 17:12:34 -0400

    added backtester and modified Oracle

--- OracleStrategy.py
+++ OracleStrategy.py
@@ -9,6 +9,7 @@ Created on Thu Apr  7 19:01:42 2022
 import tech_ind 
 import pandas as pd
 import numpy as np
+import backtester
 from matplotlib import cm
 
 class OracleStrategy:
@@ -22,27 +23,70 @@ class OracleStrategy:
 
     def test(self, start_date='2018-01-01', end_date='2019-12-31', symbol = 'DIS', starting_cash = 200000):
         
-        
         df_trades = tech_ind.get_data(start_date=start_date, end_date=end_date, symbols=[symbol], include_spy=False)
         daily_returns = df_trades.copy()
+        daily_returns.values[1:,:] = df_trades.values[1:,:] - df_trades.values[:-1, :]
+        
+        trades = df_trades.copy()
+        trades.loc[:,:] = np.NaN
+        
+        trades[daily_returns[symbol] > 0] = 1000
+        trades[daily_returns[symbol] < 0] = -1000
+        
+        # Forward fill NaNs with previous values, then fill remaining NaNs with 0.
+        trades.ffill(inplace=True)
+        trades.fillna(0, inplace=True)
+    
+        trades.iloc[1:] = trades.diff().iloc[1:]
+        trades.iloc[0] = 0        
+    
+        # And more importantly, drop all rows with no non-zero values (i.e. no trades).
+        trades = trades.loc[(trades != 0).any(axis=1)]
+    
+        """
+        # Compose the output trade list.
+        trade_list = []
+    
+        for day in trades.index:
+            if trades.loc[day,symbol] > 0:
+                trade_list.append([day.date(), symbol, 'BUY', 1000])
+            elif trades.loc[day,symbol] < 0:
+                trade_list.append([day.date(), symbol, 'SELL', 1000])
+    
+        # Dump the trades to stdout.  (Redirect to a file if you wish.)
+        for trade in trade_list:
+            print ("    ".join(str(x) for x in trade))       
+            
+            
+        
         daily_returns.values[1:,:] = abs(df_trades.values[1:,:] - df_trades.values[:-1, :]) * 1000
         daily_returns.values[0, :] = np.nan
         daily_returns['Cumulative Returns'] = (daily_returns).cumsum()
         daily_returns['Max Value'] = 200000 + daily_returns['Cumulative Returns']
-        return daily_returns
+        """
+        print(trades)
+        return trades
 
 
 if __name__ == "__main__": 
-    data = tech_ind.get_data(start_date='2018-01-01', end_date='2019-12-31', symbols =['DIS'],  include_spy=False)
+    start ='2018-10-01'
+    end = '2018-12-31'
+    data = tech_ind.get_data(start_date = start, end_date = end, symbols =['DIS'],  include_spy=False)
    
     daily_returns = (data.shift()-data) * 1000
     daily_returns.values[0, :] = np.nan
     daily_returns = daily_returns.cumsum()
     OS = OracleStrategy()
-    oracle = OS.test()
+    oracle = OS.test(start_date = start, end_date = end)
+    portfolio = backtester.assess_strategy_dataframe(oracle, start, end, starting_value = 200000, fixed_cost = 0, floating_cost = 0)
+    backtester.calc_portfolio(portfolio)
+    
+    
+    """
     base_returns = daily_returns
     base_returns.columns = ['Baseline']
     base_returns['Max Value'] = oracle['Max Value'] - 200000
     plot = base_returns.plot(title='Baseline vs. Oracle', colormap=cm.Accent)
     plot.grid()
     print(base_returns.std(axis=0))
+    """
---
+++ backtester.py
@@ -0,0 +1,102 @@
+import pandas as pd
+from assess import get_data
+import datetime
+import math as m
+
+
+def assess_strategy(trade_file = "./trades/trades.csv", starting_value = 1000000, fixed_cost = 9.95, floating_cost = 0.005):
+    trades = pd.read_csv(trade_file).sort_values(by="Date")
+    
+    #Get start and end dates
+    start_date = trades['Date'].iloc[0]
+    end_date = trades['Date'].iloc[-1]
+    
+    #Get all unique traded symbols
+    symbols = trades['Symbol'].unique().tolist()
+    
+    #get stock data
+    stocks_vals = get_data(start_date, end_date, symbols)
+    print(stocks_vals)
+    #shares = stocks_vals.index
+    shares = pd.DataFrame(index=stocks_vals.index)
+    cash_val = pd.DataFrame(index=stocks_vals.index)
+    cash_val.loc[start_date: end_date, "Cash"] = starting_value
+    
+    for idx, row in trades.iterrows():
+        cur_symbol = row['Symbol']
+        total_transaction_value = row['Shares'] * stocks_vals.loc[row['Date'], cur_symbol]
+        if (row['Direction'] == 'BUY'):
+            if cur_symbol in shares:
+                shares.loc[row['Date'] : end_date, cur_symbol] += row['Shares']
+            else:
+                shares.loc[start_date: row['Date'], cur_symbol] = 0
+                shares.loc[row['Date'] : end_date, cur_symbol] = row['Shares']
+            cash_val.loc[row['Date'] : end_date, "Cash"] -=  total_transaction_value*(1 + floating_cost) + fixed_cost
+        if (row['Direction'] == 'SELL'):
+            if cur_symbol in shares:
+                shares.loc[row['Date'] : end_date, cur_symbol] -= row['Shares']
+            else:
+                shares.loc[start_date: row['Date'], cur_symbol] = 0                
+                shares.loc[row['Date'] : end_date, cur_symbol] = -1*row['Shares']
+            cash_val.loc[row['Date'] : end_date, "Cash"] +=  total_transaction_value*(1 - floating_cost) - fixed_cost
+
+    stocks_vals = stocks_vals.drop('SPY', 1)
+    porfolio_val = pd.DataFrame(index=stocks_vals.index)
+    porfolio_val['Portfolio'] = sum(stocks_vals[sym] * shares[sym] for sym in stocks_vals) + cash_val["Cash"]
+    
+    
+    return porfolio_val
+
+def assess_strategy_dataframe(trades, start_date, end_date, starting_value = 1000000, fixed_cost = 9.95, floating_cost = 0.005):
+    
+    #Get traded symbol
+    symbol = trades.columns
+    
+    #get stock data
+    stocks_vals = get_data(start_date, end_date, symbol)
+    
+    #shares = stocks_vals.index
+    shares = pd.DataFrame(index=stocks_vals.index)
+    cash_val = pd.DataFrame(index=stocks_vals.index)
+    cash_val.loc[start_date: end_date, "Cash"] = starting_value
+    shares.loc[start_date: end_date, symbol] = 0
+    
+    print(stocks_vals)
+    print(trades)
+    for day in trades.index:
+        trade_shares = trades.loc[day,symbol].values[0]
+        total_transaction_value = trade_shares * stocks_vals.loc[day, symbol].values[0]
+        print(total_transaction_value)
+        
+        if trade_shares > 0:
+            shares.loc[day : end_date, symbol] += trade_shares
+            cash_val.loc[day : end_date, "Cash"] -=  total_transaction_value*(1 + floating_cost) + fixed_cost
+        elif trade_shares < 0:
+            shares.loc[day : end_date, symbol] -= trade_shares
+            cash_val.loc[day : end_date, "Cash"] +=  total_transaction_value*(1 - floating_cost) - fixed_cost
+    print(cash_val)
+    print(shares)
+    porfolio_val = pd.DataFrame(index=stocks_vals.index)
+    porfolio_val['Portfolio'] = sum(stocks_vals[sym] * shares[sym] for sym in stocks_vals) + cash_val["Cash"]
+    
+    
+    return porfolio_val
+
+def calc_portfolio(portfolio_val, starting_value = 1000000, risk_free_rate = 0.0, sample_freq = 252):
+    end_value = portfolio_val["Portfolio"].iloc[-1]
+    average_daily_return = ((portfolio_val["Portfolio"]/ portfolio_val["Portfolio"].shift()) - 1).mean()
+    stdev_daily_return = ((portfolio_val["Portfolio"]/ portfolio_val["Portfolio"].shift()) - 1).std()
+    cumulative_return = end_value/starting_value - 1
+    sharpe_ratio = m.sqrt(sample_freq) * ((average_daily_return - risk_free_rate)/ stdev_daily_return)
+    
+    print("Sharpe Ratio: ", sharpe_ratio)    
+    print("Volatility (stdev of daily returns): ", stdev_daily_return)
+    print("Average Daily Return: ", average_daily_return)
+    print("Cumulative Return: ", cumulative_return)
+    print("End Value: ", end_value)
+    
+    
+
+#portfolio = assess_strategy(trade_file = "trades.csv")
+#portfolio = assess_strategy()
+#calc_portfolio(portfolio)
