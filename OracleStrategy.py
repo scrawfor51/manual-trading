91fcf665b142703601ac77a27dc042f566e72db5
scrawfor51 <71202431+scrawfor51@users.noreply.github.com>
2022-04-16 20:01:28 -0400

    Add files via upload
    Oracle

--- OracleStrategy.py
+++ OracleStrategy.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr  7 19:01:42 2022
-
 @author: Stephen
 """
 
 import tech_ind 
 import pandas as pd
 import numpy as np
-import backtester
+import backtester_manual_trading as backtester
 from matplotlib import cm
 
 class OracleStrategy:
@@ -26,23 +25,26 @@ class OracleStrategy:
         df_trades = tech_ind.get_data(start_date=start_date, end_date=end_date, symbols=[symbol], include_spy=False)
         daily_returns = df_trades.copy()
         daily_returns.values[1:,:] = df_trades.values[1:,:] - df_trades.values[:-1, :]
+        daily_returns.values[0, 0] = 0
+        print("Daily Returns :", daily_returns)
+        
         
         trades = df_trades.copy()
         trades.loc[:,:] = np.NaN
         
-        trades[daily_returns[symbol] > 0] = 1000
-        trades[daily_returns[symbol] < 0] = -1000
+        trades[daily_returns[symbol].shift(-1) > 0] = 1000
+        trades[daily_returns[symbol].shift(-1) < 0] = -1000
+
         
-        # Forward fill NaNs with previous values, then fill remaining NaNs with 0.
-        trades.ffill(inplace=True)
-        trades.fillna(0, inplace=True)
-    
+      
         trades.iloc[1:] = trades.diff().iloc[1:]
-        trades.iloc[0] = 0        
-    
+        
+        print("Trades: ", trades)
+        
         # And more importantly, drop all rows with no non-zero values (i.e. no trades).
         trades = trades.loc[(trades != 0).any(axis=1)]
-    
+        trades.iloc[-1] = 0 - (trades.iloc[-2,:] / 2)
+     
         """
         # Compose the output trade list.
         trade_list = []
@@ -64,29 +66,32 @@ class OracleStrategy:
         daily_returns['Cumulative Returns'] = (daily_returns).cumsum()
         daily_returns['Max Value'] = 200000 + daily_returns['Cumulative Returns']
         """
+        
         print(trades)
         return trades
 
 
 if __name__ == "__main__": 
-    start ='2018-10-01'
-    end = '2018-12-31'
+    
+    #start_date = '2018-01-01', end_date = '2019-12-31'
+    start ='2018-01-01'
+    end = '2019-12-31'
     data = tech_ind.get_data(start_date = start, end_date = end, symbols =['DIS'],  include_spy=False)
    
     daily_returns = (data.shift()-data) * 1000
     daily_returns.values[0, :] = np.nan
     daily_returns = daily_returns.cumsum()
+    
     OS = OracleStrategy()
     oracle = OS.test(start_date = start, end_date = end)
     portfolio = backtester.assess_strategy_dataframe(oracle, start, end, starting_value = 200000, fixed_cost = 0, floating_cost = 0)
+    print(portfolio)
     backtester.calc_portfolio(portfolio)
-    
-    
-    """
     base_returns = daily_returns
+    base_returns.columns = ['Portfolio']
+    backtester.calc_portfolio(base_returns)
     base_returns.columns = ['Baseline']
-    base_returns['Max Value'] = oracle['Max Value'] - 200000
+    base_returns['Max Value'] =  portfolio - 200000
     plot = base_returns.plot(title='Baseline vs. Oracle', colormap=cm.Accent)
     plot.grid()
-    print(base_returns.std(axis=0))
-    """
+    
